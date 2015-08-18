a2f0b00b6c7a1f34dc552dd310f7a60a11b76927
bbolker <bbolker@gmail.com>
2015-08-18 17:04:00 -0400

    first (non-working) stuff for cov strucs in formulas

--- glmmTMB/R/utils.R
+++ glmmTMB/R/utils.R
@@ -31,3 +31,62 @@ reOnly <- function(f,response=FALSE) {
 ##' @note Protects against the possibility that results from deparse() will be
 ##'       split after 'width.cutoff' (by default 60, maximally 500)
 safeDeparse <- function(x, collapse=" ") paste(deparse(x, 500L), collapse=collapse)
+
+
+##' .. content for \description{} (no empty lines) ..
+##'
+##' Stolen from flexLambda branch 
+##' @title split formula containing 'specials'
+##' @param formula 
+##' @param specials 
+##' @return something
+##' @examples
+##' glmmTMB:::splitForm(~x+y+diag(f|g))
+##' @author Fabian Scheipl?
+##' @importFrom lme4 nobars
+##' @keywords internal
+splitForm <- function(formula, specials = c("diag", "cs", "ar1d")) {
+
+    ## Recursive function: (f)ind (b)ars (a)nd (s)pecials
+    ## cf. fb function in findbars (i.e. this is a little DRY)
+    fbas <- function(term) {
+        if (is.name(term) || !is.language(term)) return(NULL)
+        for (sp in specials) if (term[[1]] == as.name(sp)) return(term)
+        if (term[[1]] == as.name("(")) return(term)
+        stopifnot(is.call(term))
+        if (term[[1]] == as.name('|')) return(term)
+        if (length(term) == 2) return(fbas(term[[2]]))
+        c(fbas(term[[2]]), fbas(term[[3]]))
+    }
+    ## we don't need this
+    ## formula <- expandDoubleVerts(formula)
+                                        # split formula into separate
+                                        # random effects terms
+                                        # (including special terms)
+    formSplits <- fbas(formula)
+                                        # vector to identify what
+                                        # special (by name), or give
+                                        # "(" for standard terms
+    formSplitID <- sapply(lapply(formSplits, "[[", 1), as.character)
+                                        # standard RE terms
+    formSplitStan <- formSplits[formSplitID == "("]
+                                        # special RE terms
+    formSplitSpec <- formSplits[!(formSplitID == "(")]
+
+    if(length(formSplitSpec) == 0) stop(
+             "no special covariance structures. ",
+             "please use lmer, not flexLmer")
+
+                                        # construct the formula for
+                                        # the specials only
+    reGenerators <- as.formula(paste("~ ", paste(formSplitSpec, collapse = " + ")))   
+
+
+                                        # construct the standard
+                                        # 'lmer'-style formula
+                                        # (without the specials)
+    if(length(formSplitStan) == 0) {
+        lmerformula <- nobars(formula)
+    }
+    return(lmerformula)
+}
