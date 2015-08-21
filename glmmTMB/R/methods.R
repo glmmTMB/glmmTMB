7e87fa956dfe2fb2871ecc702f10ff48208c9a68
Arni Magnusson <arnima@hafro.is>
2015-08-21 09:25:38 +0000

    First working version of ranef

--- glmmTMB/R/methods.R
+++ glmmTMB/R/methods.R
@@ -95,11 +95,17 @@ fixef.glmmTMB <- function(object,...) {
 ##' @method ranef glmmTMB
 ##' @export
 
-ranef.glmmTMB <- function(object, condVar = FALSE, drop = FALSE,
-                          whichel = names(ans))
-{
-  bb <- obj$env$parList()$b
-  .NotYetImplemented()
+ranef.glmmTMB <- function(object,...) {
+  Z <- getME(object,"Z")
+  pl <- object$obj$env$parList(object$fit$par, object$obj$env$last.par.best)
+  ffcond <- structure(pl$beta, names = colnames(Z))
+  #FIXME: if we later let glmmTMB.R deal with rank deficient X, then go back to fixef.merMod and copy more complicated part for add.dropped=TRUE case 
+  Zzi <- getME(object,"Zzi")
+  ffzi <- structure(pl$betazi, names = colnames(Zzi))
+  ff=list("conditional model"=ffcond, "zero_inflation"=ffzi)
+  l <-sapply(ff, length)>0
+  if(sum(l)==1) return(ff[[which(l)]])
+  else return(ff[l])
 }
 
 ##' 
