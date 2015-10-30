34139cc6bc0f5749d036018f20fd4af2f511755f
bbolker <bbolker@gmail.com>
2015-10-30 19:49:36 -0400

    beginnings of confint/profile methods

--- glmmTMB/R/methods.R
+++ glmmTMB/R/methods.R
@@ -362,3 +362,48 @@ residuals.glmmTMB <- function(object, type=c("response", "pearson"),
                r/sqrt(v(fitted(object)))
            })
 }
+
+## copied from 'stats'
+
+format.perc <- function (probs, digits) {
+    paste(format(100 * probs, trim = TRUE, scientific = FALSE, digits = digits), 
+    "%")
+}
+
+##' @export
+confint.glmmTMB <- function (object, parm, level = 0.95,
+                             method=c("wald"),
+                             component= "cond", ...) 
+{
+    method <- match.arg(tolower(method))
+    cf <- unlist(fixef(object)[component])
+    pnames <- names(cf)
+    if (missing(parm)) 
+        parm <- pnames
+    else if (is.numeric(parm)) 
+        parm <- pnames[parm]
+    a <- (1 - level)/2
+    a <- c(a, 1 - a)
+    pct <- format.perc(a, 3)
+    fac <- qnorm(a)
+    ci <- array(NA, dim = c(length(parm), 2L), dimnames = list(parm, 
+        pct))
+    if (method=="wald") {
+        vv <- vcov(object)[component]
+        ss <- unlist(lapply(vv,diag))
+        ses <- sqrt(ss)[parm]
+        ci[] <- cf[parm] + ses %o% fac
+    } else {
+
+    }
+    ci
+}
+
+confint.tmbprofile <- function(object, parm=NULL, level = 0.95, ...) {
+    ## find locations of top-level (fixed + VarCorr) parameters
+}
+
+##' @importFrom TMB tmbprofile
+profile.glmmTMB <- function(fitted, trace=FALSE, ...) {
+    tmbprofile(fitted$obj, trace=trace, ...)
+}
