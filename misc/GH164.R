045974b14b95ad1d99b2a70737ed99c3a18d0c26
Ben Bolker <bbolker@gmail.com>
2016-11-15 12:11:04 -0500

    working on diagnosis for #164

--- misc/GH164.R
+++ misc/GH164.R
@@ -10,5 +10,35 @@ d <- within(d, {
             })
 
 library(glmmTMB)
-glmmTMB(y~1,data=d,family=list(family="nbinom2",link="log"))
-glmmTMB(y2~x+(1|f),data=d,family=list(family="nbinom2",link="log"))
+system.time(m1 <- glmmTMB(y~1,data=d,
+                   family=list(family="nbinom2",link="log"),verbose=TRUE))
+## 39 seconds
+system.time(m2 <- update(m1,y2~x+(1|f)))
+## Order of parameters:
+## [1] "beta"    "b"       "betazi"  "bzi"     "theta"   "thetazi" "betad"  
+## Not matching template order:
+## [1] "beta"    "betazi"  "b"       "bzi"     "betad"   "theta"   "thetazi"
+## Your parameter list has been re-ordered.
+## (Disable this warning with checkParameterOrder=FALSE)
+## Constructing atomic invpd
+## Constructing atomic invpd
+## Constructing atomic D_lgamma
+## Constructing atomic invpd
+## Constructing atomic D_lgamma
+## Error in sparseHessianFun(env, skipFixedEffects = skipFixedEffects) : 
+##   Memory allocation fail in function 'MakeADHessObject2'
+
+
+## Error in sparseHessianFun(env, skipFixedEffects = skipFixedEffects) : 
+##   Memory allocation fail in function 'MakeADHessObject2'
+
+## In addition: Warning messages:
+## 1: In nlminb(start = par, objective = fn, gradient = gr) :
+##   NA/NaN function evaluation
+## 2: In he(par) : restarting interrupted promise evaluation
+## outer mgc:  NaN 
+## Error in nlminb(start = par, objective = fn, gradient = gr) : 
+##   gradient function must return a numeric vector of length 4
+## Timing stopped at: 35.02 54.44 306.3
+## Timing stopped at: 68.77 70.27 392.1
+
