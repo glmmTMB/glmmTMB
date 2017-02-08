10bfbe983b2a4459ca76e395f60852dfb8804209
mollie brooks <mollieebrooks@gmail.com>
2017-02-08 18:59:31 +0100

    example binomial

--- glmmTMB/R/glmmTMB.R
+++ glmmTMB/R/glmmTMB.R
@@ -364,33 +364,41 @@ okWeights <- function(x) {
 ##' @importFrom stats update
 ##' @export
 ##' @examples
-##' (m1 <- glmmTMB(count~spp + mined + (1|site), zi=~spp + mined+ (1|site), family=poisson, Salamanders))
+##' (m1 <- glmmTMB(count~spp + mined + (1|site), 
+##'   zi=~spp + mined+ (1|site), 
+##'   family=poisson, data=Salamanders))
 ##' summary(m1)
 ##' ## Not run:
 ##' ## Negative binomial model
-##' (m2 <- glmmTMB(count~spp + mined + (1|site), zi=~spp + mined, family=nbinom2, Salamanders))
+##' (m2 <- glmmTMB(count~spp + mined + (1|site), 
+##'   zi=~spp + mined, 
+##'   family=nbinom2, Salamanders))
 ##' ## Dispersion model
 ##' sim1=function(nfac=40, nt=100, facsd=.1, tsd=.15, mu=0, residsd=1)
 ##' {
-##' 	dat=expand.grid(fac=factor(letters[1:nfac]), t= 1:nt)
-##' 	n=nrow(dat)
-##' 	dat$REfac=rnorm(nfac, sd= facsd)[dat$fac]
-##' 	dat$REt=rnorm(nt, sd= tsd)[dat$t]
-##' 	dat$x=rnorm(n, mean=mu, sd=residsd) + dat$REfac + dat$REt
-##' 	return(dat)
+##'   dat=expand.grid(fac=factor(letters[1:nfac]), t= 1:nt)
+##'   n=nrow(dat)
+##'   dat$REfac=rnorm(nfac, sd= facsd)[dat$fac]
+##'   dat$REt=rnorm(nt, sd= tsd)[dat$t]
+##'   dat$x=rnorm(n, mean=mu, sd=residsd) + dat$REfac + dat$REt
+##'   return(dat)
 ##' }
 ##' 
 ##' set.seed(101)
-##' d1=sim1(mu=100, residsd =10)
-##' d2=sim1(mu=200, residsd =5)
-##' d1=transform(d1,
-##' 	fac=paste0(fac, 1),
-##' 	disp="ten")
-##' d2=transform(d2,
-##' 	fac=paste0(fac, 2),
-##' 	disp="five")
+##' d1 = sim1(mu=100, residsd =10)
+##' d2 = sim1(mu=200, residsd =5)
+##' d1 = transform(d1,
+##'   fac=paste0(fac, 1),
+##'   disp="ten")
+##' d2 = transform(d2,
+##'   fac=paste0(fac, 2),
+##'   disp="five")
 ##' dat = rbind(d1, d2)
 ##' m0 = glmmTMB(x~disp+(1|t)+(1|fac), dispformula=~disp, dat)
+##' ## Binomial model
+##' data(cbpp, package="lme4")
+##' (tmbm1 <- glmmTMB(incidence/size ~ period + (1 | herd), weights=size,
+##'                data=cbpp, family=binomial))
 ##' ## End(Not run)
 glmmTMB <- function (
     formula,
