82c0d84c5d86b63298eece200e289fc8a4ef7f5b
mollie brooks <mollieebrooks@gmail.com>
2017-05-03 13:22:56 +0200

    new case studies and timing

---
+++ misc/run timing combined.R
@@ -0,0 +1,118 @@
+library(glmmTMB)
+library(ggplot2); theme_set(theme_bw())
+library(knitr)
+library(reshape)
+library(plyr)
+library(parallel)
+no_cores <- detectCores() - 1
+cl <- makeCluster(no_cores)
+clusterEvalQ(cl, {
+  library(glmmTMB)
+  library(glmmADMB)
+  library(lme4)
+  library(brms)
+  library(INLA)
+  tfun = function(x) unname(system.time(capture.output(x))["elapsed"])
+})
+
+mod = glmmTMB(count~spp * mined + (1|site), Salamanders, family="nbinom2")
+sims1 = lapply(simulate(mod, nsim=100, seed=111), 
+               function(count){ 
+                 cbind(count, Salamanders[,c('site', 'mined', 'spp')])
+               })
+
+times.tmb = do.call(c, parLapply(cl, sims1, function(x){
+  tfun(glmmTMB(count~spp * mined + (1|site), x, family="nbinom2"))}))
+
+times.admb = do.call(c, parLapply(cl, sims1, function(x){
+  tfun(glmmadmb(count~spp * mined + (1|site), x, family="nbinom2"))}))
+
+times.lme4 = do.call(c, parLapply(cl, sims1, function(x){
+  tfun(glmer.nb(count~spp * mined + (1|site), x))}))
+
+times.brms = do.call(c, parLapply(cl, sims1, function(x){
+  tfun(brm(count~spp * mined + (1|site), x, family="negbinomial"))}))
+
+times.inla = do.call(c, parLapply(cl, sims1, function(x){
+  tfun(inla(count~spp*mined+f(site, model="iid"), family= "nbinomial", data = x))}))
+
+save(times.tmb, times.admb, times.lme4, times.brms, times.inla, file="simfit.RData")
+
+simtimes = data.frame(time=c(times.tmb, times.admb, times.lme4, times.brms, times.inla),
+                      package=rep(c("glmmTMB", "glmmADMB", "lme4", "brms", "INLA"), each=length(times.tmb)),
+                      set=rep(1:length(times.tmb), 5))
+
+ggplot(simtimes)+geom_boxplot(aes(x=package, y=time))+scale_y_log10(breaks=c(5,10,50,100,500,1000))+
+  ylab("estimation time (seconds)")
+
+  
+#Benchmark with data sets of increasing size
+
+n = nrow(Salamanders)
+reps = c(1,2,4,6,8,10)
+rows = lapply(reps, function(x) rep(1:n, x))
+bigdat = lapply(rows, function(x) Salamanders[x,])
+
+times.tmb = do.call(c, parLapply(cl, bigdat, function(x){
+  tfun(glmmTMB(count~spp * mined + (1|site), x, family="nbinom2"))}))
+
+times.admb = do.call(c, parLapply(cl, bigdat, function(x){
+  tfun(glmmadmb(count~spp * mined + (1|site), x, family="nbinom2"))}))
+
+times.lme4 = do.call(c, parLapply(cl, bigdat, function(x){
+  tfun(glmer.nb(count~spp * mined + (1|site), x))}))
+
+times.brms = do.call(c, parLapply(cl, bigdat, function(x){
+  tfun(brm(count~spp * mined + (1|site), x, family="negbinomial"))}))
+
+times.inla = do.call(c, parLapply(cl, bigdat, function(x){
+  tfun(inla(count~spp*mined+f(site, model="iid"), family= "nbinomial", data = x, num.threads=1))}))
+ 
+save(times.tmb, times.admb, times.lme4, times.brms, times.inla, file="bigfit.RData")
+
+bigtimes = data.frame(time=c(times.tmb, times.admb, times.lme4, times.brms, times.inla),
+                      package=rep(c("glmmTMB", "glmmADMB", "lme4", "brms", "INLA"), each=length(times.tmb)),
+                      nobs=rep(reps*n, 5))
+
+ggplot(bigtimes, aes(x=nobs, y=time, colour=package))+
+  geom_point()+geom_smooth(method="lm", se=FALSE)+
+  scale_y_log10(breaks=c(5,10,50,100,500,1000))+
+  xlab("number of observations in data")+ylab("estimation time (seconds)")+scale_x_log10(breaks=unique(bigtimes$nobs))
+
+#Benchmark with data sets with more random effect levels
+
+n = nrow(Salamanders)
+nRE=length(unique(Salamanders$site))
+
+rows = lapply(reps, function(x) rep(1:n, x))
+bigdat = lapply(rows, function(x) Salamanders[x,])
+bigdat2 = lapply(1:length(reps), function(x) data.frame(bigdat[[x]], "grp"=paste0(bigdat[[x]]$site, rep(1:reps[x], each=n)))) 
+
+times.tmb = do.call(c, parLapply(cl, bigdat2, function(x){
+  tfun(glmmTMB(count~spp * mined + (1|grp), x, family="nbinom2"))}))
+
+times.inla = do.call(c, parLapply(cl, bigdat2, function(x){
+  tfun(inla(count~spp*mined+f(grp, model="iid"), family= "nbinomial", data = x))}))
+
+times.admb = do.call(c, parLapply(cl, bigdat, function(x){
+  tfun(glmmadmb(count~spp * mined + (1|grp), x, family="nbinom2"))}))
+
+times.lme4 = do.call(c, parLapply(cl, bigdat, function(x){
+  tfun(glmer.nb(count~spp * mined + (1| grp), x))}))
+
+times.brms = do.call(c, parLapply(cl, bigdat, function(x){
+  tfun(brm(count~spp * mined + (1| grp), x, family="negbinomial"))}))
+  
+save(times.tmb, times.inla, times.admb, times.lme4, times.brms, file="bigfitRE.RData")
+
+bigtimes = data.frame(time=c(times.tmb, times.inla, times.admb, times.lme4, times.brms),
+                      package=rep(c("glmmTMB", "INLA", "glmmADMB", "lme4", "brms"),
+                                  times=c(length(times.tmb), length(times.inla), length(times.admb), length(times.lme4), length(times.brms))),
+                      nRE=c(rep(c(1,4,16,64,256,512)*nRE, 2),
+                            rep(c(1,4,16,64)*nRE, 2),
+                            c(1,4,16)*nRE))
+
+ggplot(bigtimes, aes(x=nRE, y=time, colour=package))+
+  geom_point()+geom_smooth(method="lm", se=FALSE)+
+  scale_y_log10(breaks=c(5,10,50,100,500,1000,5000,10000))+
+  xlab("number of random effect levels (sites)")+ylab("estimation time (seconds)")+scale_x_log10(breaks=unique(bigtimes$nRE))
