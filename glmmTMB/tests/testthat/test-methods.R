1fd24c8da8191bd21f78c99a77405b7b2db0a910
Kasper Kristensen <kaskr@dtu.dk>
2017-10-25 14:36:15 +0200

    Modify test

--- glmmTMB/tests/testthat/test-methods.R
+++ glmmTMB/tests/testthat/test-methods.R
@@ -123,8 +123,8 @@ test_that("sigma", {
 })
 
 test_that("confint", {
-    ci <- confint(fm2)
-    expect_equal(ci[1:2,],
+    ci <- confint(fm2, 1:2, estimate=FALSE)
+    expect_equal(ci,
         structure(c(238.406083254105, 7.52295734348693,
                     264.404107485727, 13.4116167530013),
                   .Dim = c(2L, 2L),
