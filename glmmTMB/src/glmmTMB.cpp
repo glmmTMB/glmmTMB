1a5eb161de5a6af92ea9ee03fdef801e37e70286
mollie brooks <mollieebrooks@gmail.com>
2017-11-09 15:15:02 +0100

    dbinon and dbetabinom get  size not weights

--- glmmTMB/src/glmmTMB.cpp
+++ glmmTMB/src/glmmTMB.cpp
@@ -535,6 +535,7 @@ Type objective_function<Type>::operator() ()
   DATA_SPARSE_MATRIX(Zzi);
   DATA_MATRIX(Xd);
   DATA_VECTOR(yobs);
+  DATA_VECTOR(size); //only used in binomial
   DATA_VECTOR(weights);
   DATA_VECTOR(offset);
 
@@ -605,8 +606,8 @@ Type objective_function<Type>::operator() ()
         break;
       case binomial_family:
         s1 = logit_inverse_linkfun(eta(i), link); // logit(p)
-        tmp_loglik = dbinom_robust(yobs(i), weights(i), s1, true);
-        SIMULATE{yobs(i) = rbinom(weights(i), mu(i));}
+        tmp_loglik = dbinom_robust(yobs(i), size(i), s1, true);
+        SIMULATE{yobs(i) = rbinom(size(i), mu(i));}
         break;
       case Gamma_family:
         s1 = phi(i);           // shape
@@ -624,9 +625,9 @@ Type objective_function<Type>::operator() ()
       case betabinomial_family:
         s1 = mu(i)*phi(i); // s1 = mu(i) * mu(i) / phi(i);
         s2 = (Type(1)-mu(i))*phi(i); // phi(i) / mu(i);
-        tmp_loglik = glmmtmb::dbetabinom(yobs(i), s1, s2, weights(i), true);
+        tmp_loglik = glmmtmb::dbetabinom(yobs(i), s1, s2, size(i), true);
         SIMULATE {
-          yobs(i) = rbinom(weights(i), rbeta(s1, s2) );
+          yobs(i) = rbinom(size(i), rbeta(s1, s2) );
         }
         break;
       case nbinom1_family:
