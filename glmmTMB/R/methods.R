7bec05d6f336227eadd854fc80928e96beb802f3
Ben Bolker <bbolker@gmail.com>
2018-12-18 16:51:43 -0500

    switch to TMB::tmbroot

--- glmmTMB/R/methods.R
+++ glmmTMB/R/methods.R
@@ -679,7 +679,7 @@ format.perc <- function (probs, digits) {
 ##' @param ncpus number of CPUs/cores to use for parallel computation
 ##' @param cl cluster to use for parallel computation
 ##' @param ... arguments may be passed to \code{\link{profile.merMod}} or
-##' \code{\link{tmbroot}}
+##' \code{\link[TMB]{tmbroot}}
 ##' @examples
 ##' data(sleepstudy, package="lme4")
 ##' model <- glmmTMB(Reaction ~ Days + (1|Subject), sleepstudy)
@@ -800,7 +800,7 @@ confint.glmmTMB <- function (object, parm, level = 0.95,
         parallel <- plist$parallel
         do_parallel <- plist$do_parallel
         FUN <- function(n) {
-            tmbroot(obj=object$obj, name=n, target=0.5*qchisq(level,df=1),
+            TMB::tmbroot(obj=object$obj, name=n, target=0.5*qchisq(level,df=1),
                     ...)
         }
         if (do_parallel) {
