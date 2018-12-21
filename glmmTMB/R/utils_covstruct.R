39b91950565c77b3d12a3f7fe70863dbde36ac25
Ben Bolker <bbolker@gmail.com>
2018-12-21 11:38:57 -0500

    fix double-|| issue in numFactor

--- glmmTMB/R/utils_covstruct.R
+++ glmmTMB/R/utils_covstruct.R
@@ -32,8 +32,7 @@
 ##' @export
 numFactor <- function(x, ...) {
     y <- data.frame(x, ...)
-    if( !all( sapply(y, is.numeric) ||
-              sapply(y, is.factor)) )
+    if( !all( sapply(y, is.numeric) | sapply(y, is.factor)) )
         stop("All arguments to 'numFactor' must be numeric or factor.")
     asChar <- function(y) {
         y <- lapply(y, as.character)
