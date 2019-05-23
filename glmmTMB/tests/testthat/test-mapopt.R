a5d60e184e9e458397ab1041ed5c16ace987d778
Ben Bolker <bbolker@gmail.com>
2019-05-23 13:51:15 -0400

    tests for mapping + alt opt

---
+++ glmmTMB/tests/testthat/test-mapopt.R
@@ -0,0 +1,32 @@
+stopifnot(require("testthat"),
+          require("glmmTMB"))
+
+data(Salamanders, package = "glmmTMB")
+
+context("mapping")
+
+m1 <- glmmTMB(count~ mined, family=poisson, data=Salamanders,
+              start=list(beta=c(0,2)),
+              map=list(beta=factor(c(1,NA))))
+
+m1optim <- update(m1, control=glmmTMBControl(optimizer=optim,
+                                             optArgs=list(method="BFGS")))
+
+test_that("basic mapping works",
+          expect_equal(fixef(m1)$cond[[2]], 2.0))
+
+test_that("predict works with mapped params",
+          expect_equal(vapply(predict(m1,se.fit=TRUE),unique,numeric(1)),
+                       c(fit = -1.18646939995962, se.fit = 0.0342594326326737),
+                       tolerance=1e-6))
+
+context("alternate optimizers")
+
+test_that("alternate optimizers work", {
+          expect_equal(fixef(m1),fixef(m1optim),
+                       tol=1e-4)
+          expect_false(identical(fixef(m1),fixef(m1optim)))
+})
+          
+
+        
