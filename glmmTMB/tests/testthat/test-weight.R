865f0db66e0ce7952385afc8677c31878f62b47f
Daniel Lüdecke <mail@danielluedecke.de>
2019-06-11 09:42:14 +0200

    restore white space

--- glmmTMB/tests/testthat/test-weight.R
+++ glmmTMB/tests/testthat/test-weight.R
@@ -5,19 +5,19 @@ stopifnot(require("testthat"),
 context("weight")
 
 
-set.seed(1)
+set.seed(1) 
 nrep = 20
 nsim = 5
 sdi = .1
 sdii = .2
 rho = -.1
-slope = .8
+slope = .8 
 ni=100
 
 dat = expand.grid(i=1:ni, rep=1:nrep , x=c(0 ,.2, .4))
-RE = MASS::mvrnorm(n = ni, mu =c(0, 0),
-		Sigma = matrix(c(sdi*sdi, rho*sdi*sdii, rho*sdi*sdii ,sdii*sdii),2,2))
-inddat = transform(dat, y=rpois(n=nrow(dat),
+RE = MASS::mvrnorm(n = ni, mu =c(0, 0), 
+		Sigma = matrix(c(sdi*sdi, rho*sdi*sdii, rho*sdi*sdii ,sdii*sdii),2,2)) 
+inddat = transform(dat, y=rpois(n=nrow(dat), 
                                 lambda = exp(RE[i,1] + x*(slope + RE[i,2]))))
 
 ## aggdat = ddply(inddat, ~i+x+y, summarize, freq=length(rep))
