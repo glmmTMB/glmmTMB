914d63e7b0a6060bda1621317e1889b337d5eca3
Ben Bolker <bbolker@gmail.com>
2020-06-07 14:16:24 -0400

    adjust optim() output

---
+++ glmmTMB/tests/testthat/test-altopt.R
@@ -0,0 +1,16 @@
+stopifnot(require("testthat"),
+          require("glmmTMB"))
+
+data(sleepstudy, cbpp,
+     package = "lme4")
+
+m1 <- glmmTMB(count~ mined, family = poisson, data = Salamanders,
+              control = glmmTMBControl(optimizer = optim,
+                                       optArgs = list(method="BFGS")))
+
+context("alternative optimizers")
+
+test_that("downstream methods work with optim()", {
+    expect_is(summary(m1),"summary.glmmTMB")
+    expect_is(confint(m1),"matrix")
+})
