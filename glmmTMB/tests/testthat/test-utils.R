bb24de009a2808aa0cbf2688815c6f9298c1b8cf
Ben Bolker <bbolker@gmail.com>
2020-06-15 17:49:24 -0400

    make noSpecials work with one-sided formulas; add tests

--- glmmTMB/tests/testthat/test-utils.R
+++ glmmTMB/tests/testthat/test-utils.R
@@ -6,3 +6,13 @@ test_that("addForm", {
     expect_warning(addForm(y~x,z~1),
                    "discarding LHS")
 })
+
+test_that("noSpecials", {
+    expect_equal(noSpecials(y~1+us(1|f)), y~1)
+    expect_equal(noSpecials(y~1+us(1|f),delete=FALSE), y~1+(1|f))
+    expect_equal(noSpecials(y~us(1|f)), y ~ 1)
+    expect_equal(noSpecials(y~us(1|f), delete=FALSE), y~ (1|f))
+    expect_equal(noSpecials(y~us+1), y ~ us + 1)
+    expect_equal(noSpecials(~us+1), ~ us + 1)
+    expect_equal(noSpecials(~1+x+us(1|f), delete=FALSE), ~ 1 + x + (1|f))
+})
