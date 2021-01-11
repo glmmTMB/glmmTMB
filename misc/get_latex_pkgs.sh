eb5cd400f11b650b42692e166d03ff087f4d215f
Ben Bolker <bbolker@gmail.com>
2021-01-11 10:06:50 -0500

    latex pkgs script [skip ci]

--- misc/get_latex_pkgs.sh
+++ misc/get_latex_pkgs.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 ## run from head directory
+## this isolates all of the \usepackage{} packages into
+## a quoted, comma-separated list
+## missing: chicago.bst -> chicago, american.ldf -> babel-english
 ls glmmTMB/vignettes/*.Rnw | \
     xargs grep usepackage | \
     egrep -v '% *\\' | \
