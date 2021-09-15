7145cc5b4104d4d44057710311a716646e47b334
Ben Bolker <bbolker@gmail.com>
2021-09-15 16:29:21 -0400

    loosen test for Solaris (#732)

--- glmmTMB/tests/testthat/test-sparseX.R
+++ glmmTMB/tests/testthat/test-sparseX.R
@@ -7,10 +7,11 @@ context("sparse X models")
 
 test_that("basic fits", {
     fm2S <- update(fm2,sparseX=c(cond=TRUE))
-    expect_equal(fixef(fm2),fixef(fm2S), tolerance=1e-6)
-    expect_equal(VarCorr(fm2),VarCorr(fm2S),tolerance=1e-3)
-    expect_equal(dim(getME(fm2,"X")),dim(getME(fm2S,"X")))
-    expect_equal(predict(fm2),predict(fm2S), tolerance=1e-3)
+    ## loosened from 1e-6 to 2e-6 for Solaris ...
+    expect_equal(fixef(fm2), fixef(fm2S), tolerance=2e-6)
+    expect_equal(VarCorr(fm2), VarCorr(fm2S),tolerance=1e-3)
+    expect_equal(dim(getME(fm2,"X")), dim(getME(fm2S,"X")))
+    expect_equal(predict(fm2), predict(fm2S), tolerance=1e-3)
     nd <- sleepstudy[1,]
     expect_equal(predict(fm2, newdata=nd),predict(fm2S, newdata=nd),
                  tolerance=1e-3)
