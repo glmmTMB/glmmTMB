3684cb550e5d54a8fa280e49663a94ffa6a4a862
Ben Bolker <bbolker@gmail.com>
2021-12-08 18:41:23 -0500

    more tnb variance notes

--- misc/tnb2_ex.R
+++ misc/tnb2_ex.R
@@ -6,8 +6,17 @@ dd <- dd[dd$y >= 3, , drop=FALSE]
 ## everything to work properly (e.g. Pearson residuals)
 ## conditional variance of a truncated distribution?
 ##  could do it by brute force:
-##  sum(p(x)*x^2) = (full_var - sum_{x=0 to trunc_pt}(p(x)*x^2))/(1-sum_{x=0 to trunc_pt}(p(x)*x))
 ##   https://data.princeton.edu/wws509/notes/countmoments
+## NOT correct: need to go through this in more detail
+##  variance = function(mu, phi, trunc_val = 2) {
+##    nb_var <- mu * (1 + mu / phi)
+##    trunc_mass <- pnbinom(trunc_val, mu = mu, size = phi)
+##    i <- sseq(0, trunc_val)
+##    mean_miss <- sum(i * dnbinom(i, mu  = mu, size = phi))
+##    var_miss <- sum(i^2 * dnbinom(i, mu  = mu, size = phi))
+##    return((nb_var - var_miss + mean_miss^2)/(1-trunc_mass))
+## })
+
 ff <- glmmTMB:::family_factory("log", "truncated_nbinom2_2", variance = NULL)
 
 library(glmmTMB)
