3bfad648ea77682d9e832ccf7635165e96b3cbc2
Ben Bolker <bbolker@gmail.com>
2022-10-05 10:55:39 -0400

    tests for trunc prediction

--- misc/trunc_predict.R
+++ misc/trunc_predict.R
@@ -16,7 +16,7 @@ df <- data.frame(p1 = rpois(N, 1),
         tp1 = ifelse(p1 == 0, NA, p1),
         tnb1 = ifelse(nb1 == 0, NA, nb1))
 
-testfun <- function(model, response) {
+testfun <- function(model, response, distrib) {
     zp1 <- predict(model, type="zprob")
     cm1 <- predict(model, type="conditional")
     mu1 <- predict(model, type="response")
@@ -32,15 +32,22 @@ testfun <- function(model, response) {
     ## mean of non-zero values == conditional mean
     ## (for all truncated distributions)
     expect_equal(mean(df[[response]][nzvals]),
-                 mean(cm1),
-                 tolerance = 1e-2)
+    mean(cm1),
+    tolerance = 1e-2)
+
+    ## compute zero-trunc by hand
+    eta <- predict(model, type = "link")
+    cm2 <- exp(eta)/(1-distrib(0, exp(eta), sigma(model)))
+    expect_equal(cm1, cm2)
 
     ## mean of all values == response
-    expect_equal(mean(df[[response]]),
-                 mean(mu1),
-                 tolerance = 5e-3)
+    ## expect_equal(mean(df[[response]]),
+    ##              mean(mu1),
+    ##              tolerance = 5e-3)
 }
 
+my_dpois <- function(x, lambda, ...) dpois(x, lambda)
+my_nb2 <- function(x, mu, size) dnbinom(x, mu = mu, size = size)
 
 f_zp1 <- glmmTMB(zp1 ~ x,
                  zi= ~ 1,
@@ -48,13 +55,13 @@ f_zp1 <- glmmTMB(zp1 ~ x,
                  data=df)
 
 
-testfun(f_zp1, "zp1")
+testfun(f_zp1, "zp1", my_dpois)
 
 f_znb2 <- update(f_zp1, znb1 ~ .,
                  family = truncated_nbinom2)
 
 ## hmm, fails for nbinom2??
-testfun(f_znb2, "znb1")
+testfun(f_znb2, "znb1", my_nb2)
 ## calculate by hand
 cm1 <- predict(f_znb2, type="conditional")
 eta <- predict(f_znb2, type = "link")
@@ -68,3 +75,10 @@ all.equal(mval(exp(eta), sigma(f_znb2)), cm1)
 f_znb1 <- update(f_zp1, znb1 ~ .,
                  family = truncated_nbinom1)
 testfun(f_znb1, "znb1")  ## works.
+
+f_tp1 <- update(f_zp1, tp1 ~ .,
+                 ziformula = ~0,
+                 family = truncated_poisson)
+expect_equal(predict(f_tp1, type = "conditional"),
+             predict(f_tp1, type = "response"))
+expect_true(all(predict(f_tp1, type = "zprob") == 0))
