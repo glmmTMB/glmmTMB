dce3778c0701e6c2fc16f9b2391718b541321286
Ben Bolker <bbolker@gmail.com>
2023-08-05 19:52:22 -0400

    fix tests

--- glmmTMB/R/glmmTMB.R
+++ glmmTMB/R/glmmTMB.R
@@ -386,9 +386,11 @@ mkTMBStruc <- function(formula, ziformula, dispformula,
                   disp = get_fixnm(dispList))
   ## need 'cnms' (names to localize and lengths to figure out ncoef)
   ## 'ss' (structure)  
-  get_reinfo <- function(x) list(cnms = x$reTrms[["cnms"]], ss = x[["ss"]])
-  re_info <- list(cond = get_reinfo(condList),
-                  zi = get_reinfo(ziList))
+  get_reinfo <- function(x, y) {
+      list(cnms = x$reTrms[["cnms"]], ss = x[["ss"]], y)
+  }
+  re_info <- list(cond = get_reinfo(condList, condReStruc),
+                  zi = get_reinfo(ziList, ziReStruc))
 
   ## easy way to get lengths of theta of individual components??
   prior_struc <- proc_priors(priors, info = list(fix = fix_nms, re = re_info))
--- glmmTMB/tests/testthat/test-priors.R
+++ glmmTMB/tests/testthat/test-priors.R
@@ -1,6 +1,6 @@
 stopifnot(require("testthat"),
           require("glmmTMB"))
-source("tests/testthat/setup_makeex.R")
+## source("tests/testthat/setup_makeex.R")
 cprior1 <- data.frame(prior = "normal(0, 3)",
                      class = "beta",
                      coef = "")
@@ -14,14 +14,16 @@ cprior3 <- data.frame(prior = "normal(0, 3)",
                      coef = 2)
 
 cprior4 <- data.frame(prior = "gamma(1e8, 2)",
-                     class = "theta",
-                     coef = 2)
+                     class = "ranef_pars",
+                     coef = "1|herd")
 
 gm0p1 <- update(gm0, priors = cprior1)
 gm1p2 <- update(gm1, priors = cprior2)
 gm1p3 <- update(gm1, priors = cprior3)
 
 try(update(gm1, priors = cprior4))
+
+## update(gm1, priors = cprior4)
 
 get_prior_info <- function(fit) {
     pp <- fit$obj$env$data
