1f518d02e657964ab76326e9dba80c4e9ff3fa5b
Ben Bolker <bbolker@gmail.com>
2023-10-10 19:48:49 -0400

    working on reformulas for smooths

---
+++ misc/GH928.R
@@ -0,0 +1,37 @@
+## https://github.com/glmmTMB/glmmTMB/issues/928
+## issues with smooths
+
+devtools::load_all("glmmTMB")
+
+## handle NA values properly (use `fr` rather than `mf$data` to adjust smooth)
+library(glmmTMB)
+data(sleepstudy, package = "lme4")
+ss <- sleepstudy
+ss[3, "Days"] <- NA
+ss$d <- ss$Days+1
+m1 <- glmmTMB(Reaction ~ s(Days), sleepstudy)
+m2 <- update(m1, data = ss)
+
+
+m3 <- update(m1, . ~ s(log(Days+1)))
+## Error in names(dat) <- object$term : 
+##   'names' attribute [1] must be the same length as the vector [0]
+
+m4 <- update(m1, . ~ s(log(d)), data = ss) ## OK
+
+## OK
+m5 <- update(m1, . ~ s(log(d), k = 4), data = ss)
+
+f <- function(x, ...) glmmTMB:::findbars_x(x, default.special = NULL, target = "s", ...)
+f(Reaction ~ s(log(Days)))
+## OK: s(log(Days))
+f(Reaction ~ s(log(Days+1)))
+## not OK: list of s(log(`+`)), s(log(Days)), s(log(1))
+f(Reaction ~ s(log(d), k = 4))  ## OK
+
+
+
+## this does work though ...
+f(y ~ a + log(b) + s(x, bs = "tp") + s(y, bs = "gp"))
+f(y ~ a + log(b) + s(x, k = 5))  ## works
+
