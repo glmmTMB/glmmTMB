27cfb92a6da230239198b46d0886e228e86de1d9
Ben Bolker <bbolker@gmail.com>
2024-01-04 17:11:51 -0500

    git commit chaff

--- glmmTMB/tests/testthat/test-checkRank.R
+++ glmmTMB/tests/testthat/test-checkRank.R
@@ -170,26 +170,18 @@ test_that("equivalence between 'skip' and 'warn' when confronted with identifiab
         "fixed effects in dispersion model are rank deficient"
     )
     expect_equal(fixef(m1), fixef(m2))
-<<<<<<< HEAD
-    # sparse X
-=======
     ## sparse X
     ## small-eig warning
->>>>>>> master
     suppressWarnings(m1 <- glmmTMB(y ~ x1 + x2 + x3 + x4, data=dat, control=cc1))
     expect_warning(
         m2 <- glmmTMB(y ~ x1 + x2 + x3 + x4, data=dat, control=glmmTMBControl(rank_check='warn'), sparseX=c(cond=TRUE)),
         "fixed effects in conditional model are rank deficient"
     )
     expect_equal(fixef(m1), fixef(m2))
-<<<<<<< HEAD
-    m1 <- suppressWarnings(glmmTMB(y ~ 1, ziformula = ~ x1 + x2 + x3 + x4, data=dat, control = cc1, sparseX=c(zi=TRUE)))
-=======
     ## small-eig warning
     suppressWarnings(
         m1 <- glmmTMB(y ~ 1, ziformula = ~ x1 + x2 + x3 + x4, data=dat, control = cc1, sparseX=c(zi=TRUE))
     )
->>>>>>> master
     expect_warning(
         m2 <- glmmTMB(y ~ 1, ziformula = ~ x1 + x2 + x3 + x4, data=dat, control=glmmTMBControl(rank_check='warn'), sparseX=c(zi=TRUE)),
         "fixed effects in zero-inflation model are rank deficient"
