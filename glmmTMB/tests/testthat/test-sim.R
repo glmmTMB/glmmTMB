eafda171f8266586a6c4638209a75d1878a2efb1
Ben Bolker <bbolker@gmail.com>
2024-01-05 10:31:46 -0500

    working on b tests

--- glmmTMB/tests/testthat/test-sim.R
+++ glmmTMB/tests/testthat/test-sim.R
@@ -7,14 +7,13 @@ pp <- list(beta = c(280, 3),
            theta = c(3, 0.5, 2))
 
 
+s1 <- simulate_new( ~ Days + (Days|Subject),
+                   seed = 101,
+                   newdata = sleepstudy,
+                   family = gaussian,
+                   newparams = pp)[[1]]
+
 test_that("basic simulate_new",  {
-    s1 <- simulate_new( ~ Days + (Days|Subject),
-                       seed = 101,
-                       newdata = sleepstudy,
-                       family = gaussian,
-                       newparams = pp)[[1]]
-    ## plot(sleepstudy$Days, s1, ylim = c(180,450))
-    ## with(sleepstudy, points(Days, Reaction, col = 2))
     expect_equal(s1, c(273.908009267299, 278.879666041338,
                        274.76059639018, 283.450108370691, 
                        286.469055736181, 290.145189139373))
@@ -58,46 +57,50 @@ s4 <- simulate_new( ~ Days + (Days|Subject),
                    newdata = sleepstudy,
                    family = gaussian,
                    newparams = pp2)[[1]]
-plot(sleepstudy$Days, s4, ylim = c(180,450))
-with(sleepstudy, points(Days, Reaction, col = 2))
-points(sleepstudy$Days, s1, col = 4)
 
-s5 <- simulate_new( ~ Days + (Days|Subject),
-                   seed = 101,
-                   newdata = sleepstudy,
-                   family = gaussian,
-                   newparams = pp2,
-                   return_val = "pars")
+test_that("simulate_new with specified b (values)", {
+    ## should be different from random-b sim
+    expect_false(isTRUE(all.equal(s1, s4)))
+})
 
-print(s5)
+test_that("simulate_new with specified b (pars)", {
+    s5 <- simulate_new( ~ Days + (Days|Subject),
+                       seed = 101,
+                       newdata = sleepstudy,
+                       family = gaussian,
+                       newparams = pp2,
+                       return_val = "pars")
+    expect_identical(unname(s5[names(s5)=="b"]), pp2$b)
+})                    
 
-s6 <- simulate_new( ~ Days + (Days|Subject),
-                   seed = 101,
-                   newdata = sleepstudy,
-                   family = gaussian,
-                   newparams = pp2,
-                   return_val = "object")
-
-getME(s6, "b")  ## correct, although names are wrong?
+test_that("simulate_new with specified b (object)", {
+    s6 <- simulate_new( ~ Days + (Days|Subject),
+                       seed = 101,
+                       newdata = sleepstudy,
+                       family = gaussian,
+                       newparams = pp2,
+                       return_val = "object")
+    expect_identical(unname(getME(s6, "b")),
+                     ## correct, although names are wrong?
+                     pp2$b)
+})
 
 pp3 <- c(pp, list(b = list("Days|Subject" = pp2$b)))
 
-s7 <- simulate_new( ~ Days + (Days|Subject),
-                   seed = 101,
-                   newdata = sleepstudy,
-                   family = gaussian,
-                   newparams = pp3,
-                   return_val = "pars")
-## same as before
-
-## try some examples with multiple b terms ...
+test_that("simulate_new with specified b (pars, b as list)", {
+    s7 <- simulate_new( ~ Days + (Days|Subject),
+                       seed = 101,
+                       newdata = sleepstudy,
+                       family = gaussian,
+                       newparams = pp3,
+                       return_val = "pars")
+    expect_identical(unname(s5[names(s5)=="b"]), pp3$b[["Days|Subject"]])
+})                    
 
 pp4 <- list(beta = c(280),
            betad = 1,
            theta = c(-1, 1, 0))
 
-
-## basic sim (checking for sensible parameters)
 s8 <- simulate_new( ~ 1 + (1|Subject) + ar1(0+factor(Days)|Subject),
                    seed = 101,
                    newdata = sleepstudy,
@@ -111,30 +114,70 @@ s9 <- simulate_new( ~ 1 + (1|Subject) + ar1(0+factor(Days)|Subject),
                    newparams = pp4,
                    return_val = "pars")
 
+testthat("simulate_new with two RE terms", {
+    expect_equal(unname(head(s9)),
+                 c(-0.119942121932298, 0.203239358640131, -0.248297964136576, 
+                   0.078858438002708, 0.114325605998615, 0.431878061822679))
+
+})
+
 nb <- sum(names(s9) == "b")
 pp5 <- c(pp4, list(b = rep(c(-1, 0, 1), length.out = nb)))
-## basic sim (checking for sensible parameters)
-s10 <- simulate_new( ~ 1 + (1|Subject) + ar1(0+factor(Days)|Subject),
-                   seed = 101,
-                   newdata = sleepstudy,
-                   family = gaussian,
-                   newparams = pp5)[[1]]
 
+s10 <- simulate_new( ~ 1 + (1|Subject) + ar1(0+factor(Days)|Subject),
+                    seed = 101,
+                    newdata = sleepstudy,
+                    family = gaussian,
+                    newparams = pp5)[[1]]
+
+testthat("simulate_new with two RE terms, fixed b", {
+    s11 <- simulate_new( ~ 1 + (1|Subject) + ar1(0+factor(Days)|Subject),
+                        seed = 101,
+                        newdata = sleepstudy,
+                        family = gaussian,
+                        newparams = pp5, return_val = "pars")
+    expect_equal(unname(s11[names(s11) == "b"]), pp5$b)
+})
 
 ns <- length(unique(sleepstudy$Subject))
 pp6 <- c(pp4, list(b = list("0+factor(Days)|Subject" =
                                 rep(c(-1, 0, 1), length.out = nb - ns))))
 
-s11 <- simulate_new( ~ 1 + (1|Subject) + ar1(0+factor(Days)|Subject),
+
+s12 <- simulate_new( ~ 1 + (1|Subject) + ar1(0+factor(Days)|Subject),
                    seed = 101,
                    newdata = sleepstudy,
                    family = gaussian,
                    newparams = pp6)[[1]]
 
-s12 <- simulate_new( ~ 1 + (1|Subject) + ar1(0+factor(Days)|Subject),
+testthat("simulate_new, b partially fixed", {
+    expect_false(isTRUE(all.equal(s10, s12)))
+})    
+
+s13 <- simulate_new( ~ 1 + (1|Subject) + ar1(0+factor(Days)|Subject),
                    seed = 101,
                    newdata = sleepstudy,
                    family = gaussian,
                    newparams = pp6,
                    return_val = "pars")
 
+testthat("simulate_new, b partially fixed (pars)", {
+    bvec <- unname(s13[names(s13) == "b"])
+    expect_equal(head(bvec),
+                 c(-0.119942121932298, 0.203239358640131, -0.248297964136576, 
+                   0.078858438002708, 0.114325605998615, 0.431878061822679))
+    expect_identical(bvec[-(1:ns)], pp6$b[[1]])
+})
+
+## s14 <- simulate_new( ~ 1 + (1|Subject) + ar1(0+factor(Days)|Subject),
+##                    seed = 101,
+##                    newdata = sleepstudy,
+##                    family = gaussian,
+##                    newparams = pp6,
+##                    return_val = "object")
+
+## Problem with these parameter entries:
+## b 
+## 3 
+## Error in MakeADFun(data.tmb, parameters, map = mapArg, random = randomArg,  : 
+##   Only numeric matrices, vectors and arrays can be interfaced
