ddf7d25b4e7d50174c58a6aadfbb5efbbbfd94e0
Ben Bolker <bbolker@gmail.com>
2025-03-28 15:02:18 -0400

    autopar check specifies DLL

--- glmmTMB/R/utils.R
+++ glmmTMB/R/utils.R
@@ -1041,5 +1041,5 @@ our_binom_initialize <- function(family) {
 
 #' retrieve current value of TMB autopar setting
 get_autopar <- function() {
-    attr(TMB::openmp(), "autopar")
+    attr(TMB::openmp(), "autopar", DLL = "glmmTMB")
 }
