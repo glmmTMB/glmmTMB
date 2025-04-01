9f91d24cf8d954df7d1fb54d9730fb18d2426e8c
Ben Bolker <bbolker@gmail.com>
2025-04-01 12:43:02 -0400

    start hacking troubleshooting

--- glmmTMB/inst/vignette_data/sherman.R
+++ glmmTMB/inst/vignette_data/sherman.R
@@ -1,12 +1,19 @@
 library(glmmTMB)
-load("sherman.rda")
 ## dd <- readxl::read_excel("TMB_Data.xlsx",na="NA")
 ## dd <- dd[,c("taeCPUE","Avg.Relief","site.name","reef.name")]
 
 mod1 <- glmmTMB(formula = taeCPUE ~ Avg.Relief + (1 | site.name/reef.name), 
                 data = dd, family = nbinom1, ziformula = ~1, dispformula = ~1)
+mod1_sum <- summary(mod1)
+
+mod1_diag <- capture.output(diagnose(mod1))
+
 ## warning: non-integer counts ...
 mod2 <- update(mod1, ziformula=~0)
+mod2_sum <- summary(mod2)
+
+mod2_diag <- capture.output(diagnose(mod1))
+
 mod2_optim <- update(mod2,
                 control=glmmTMBControl(optimizer=optim,
                                        optArgs=list(method="BFGS")))
@@ -14,4 +21,4 @@ mod2_optim <- update(mod2,
 ## non-integer counts + 'false convergence'
 mod3 <- update(mod2, family=poisson)
 ## non-integer counts
-save("mod1","mod2","mod2_optim","mod3","dd",version=2, file="sherman.rda")
+save("mod1_sum","mod2_sum","mod1_diag", "mod2_optim","mod3", version=2, file="sherman.rda")
