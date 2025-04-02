d403164c9e1078296f253215756242d2a2540983
Ben Bolker <bbolker@gmail.com>
2025-04-02 09:18:41 -0400

    update qra debug example

--- misc/qra_debug.R
+++ misc/qra_debug.R
@@ -1,5 +1,22 @@
-library(qra)
+data("HawCon", package = "qra")
 library(glmmTMB)
-HCbin.cll <- readRDS("HCbin.cll.rds")
-qra::extractLT(p=0.99, obj=HCbin.cll, 
-               a=1:8, b=9:16, eps=0, df.t=NULL)[,-2]
+library(splines)
+## devtools::load_all("~/R/pkgs/glmmTMB/glmmTMB")
+
+## load/process data from qra package
+CCnum <- match("CommonName", names(HawCon))
+names(HawCon)[CCnum] <- "CN"
+HawCon <- within(HawCon, {
+  trtGp <- factor(paste0(CN,LifestageTrt, sep=":"))
+  trtGpRep <- paste0(CN,LifestageTrt,":",RepNumber)
+  scTime <- scale(TrtTime)
+  obs <- factor(1:nrow(HawCon))
+})
+
+form <- cbind(Dead,Live)~0+trtGp/TrtTime+(1|trtGpRep)
+HCbb.cll <- glmmTMB::glmmTMB(form, dispformula=~trtGp+ns(scTime,2), 
+                    family=glmmTMB::betabinomial(link="cloglog"),
+                    data=HawCon)
+vv0 <- vcov(HCbb.cll)
+HCbin.cll <- update(HCbb.cll, family=binomial(link="cloglog"))
+vv1 <- vcov(HCbin.cll)
