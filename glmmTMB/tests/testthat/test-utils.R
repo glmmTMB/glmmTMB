fc0e6d8489cc58c171c8648c090ae47b925cb224
Ben Bolker <bbolker@gmail.com>
2026-09-21 11:26:07 -0400

    clean up winbuilder issues (quote types in test, stale link)

--- glmmTMB/tests/testthat/test-utils.R
+++ glmmTMB/tests/testthat/test-utils.R
@@ -77,10 +77,10 @@ test_that("up2date doesn't mangle parameter order", {
 test_that("check_dots", {
     expect_null(check_dots())
     expect_null(check_dots(foo = 1, .ignore = "foo"))
-    expect_error(check_dots(foo = 1, bar = 2), "'foo','bar'")
+    expect_error(check_dots(foo = 1, bar = 2), ".foo.,.bar.")
     expect_warning(check_dots(foo = 1, .action = "warning"),
-                   "unknown arguments ignored: 'foo'")
+                   "unknown arguments ignored: .foo.")
     expect_message(check_dots(foo = 1, .action = "message"),
-                    "unknown arguments ignored: 'foo'")
+                    "unknown arguments ignored: .foo.")
 })
 
